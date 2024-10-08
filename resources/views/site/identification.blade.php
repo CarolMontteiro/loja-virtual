8f0cf883b84ad4ca945f7cf2bedec9247794e1f1
CarolMontteiro <carolmonteiro1206@gmail.com>
2024-10-08 16:54:13 -0300

    Adicionando responsividade da página de Identificação

--- resources/views/site/identification.blade.php
+++ resources/views/site/identification.blade.php
@@ -1,30 +1,33 @@
 @extends('site.template')
 @section('content')
-<section id="identification">
+<section id="identification" class="pb-20">
     <div class="container">
-        <h1 class="text-blue-800 text-2xl text-center font-semibold uppercase sm:mt-5 py-10">Identificação</h1>
-        <form class="bg-white flex justify-between py-10">
-            <div class="create-account border-r-2 w-1/2 text-center">
-                <h2 class="text-blue-800 text-2xl text-center font-semibold pb-10">Crie uma conta</h2>
-                <fieldset>
-                    <label for="email-acconunt" class="text-lg mr-2">Email</label>
-                    <input type="email" id="email-acconunt" class="w-1/2 border-2 rounded-lg p-1.5 outline-none">
+        <h1 class="text-blue-800 text-lg text-center font-semibold uppercase py-6 sm:text-xl sm:py-10 md:text-2xl">Identificação</h1>
+        <form class="bg-white py-10 md:flex md:justify-between">
+            <div class="create-account text-center border-b-2 pb-10 md:border-r-2 md:border-b-0 md:w-1/2 ">
+                <h2 class="text-blue-800 text-xl text-center font-semibold pb-6 md:pb-10 md:text-2xl">Crie uma conta</h2>
+                <fieldset class="w-4/5 mx-auto sm:w-3/5">
+                    <label for="email-acconunt" class="text-lg pb-1 mr-2 text-gray-600 text-start block">Email</label>
+                    <input type="email" id="email-acconunt" class="required w-full border-2 rounded-lg p-1.5 outline-none">
+                    <span class="span-required"></span>
                 </fieldset>
 
                 <button type="submit" class="bg-lime-600 text-white text-lg font-semibold mt-4 py-2 px-16 rounded-lg hover:bg-blue-800 hover:text-yellow-400">Continuar</button>
                 <span class="block mt-5 text-gray-600">Dúvidas?<a href="#" class="underline hover:text-blue-800"> Fale conosco</a></span>
             </div>
 
-            <div class="login w-1/2 text-center">
-                <h2 class="text-blue-800 text-2xl text-center font-semibold pb-10">Já sou cliente</h2>
-                <fieldset>
-                    <label for="email-login" class="text-lg mr-2">Email</label>
-                    <input type="text" id="email-login" class="w-1/2 border-2 rounded-lg mb-6 p-1.5 outline-none">
+            <div class="login text-center md:w-1/2">
+                <h2 class="text-blue-800 text-2xl text-center font-semibold pt-10 pb-6 md:pb-10">Já sou cliente</h2>
+                <fieldset class="w-4/5 mx-auto sm:w-3/5">
+                    <label for="email-login" class="text-lg pb-1 mr-2 text-gray-600 text-start block">Email</label>
+                    <input type="text" id="email-login" class="required w-full border-2 rounded-lg mb-6 p-1.5 outline-none">
+                    <span class="span-required"></span>
                 </fieldset>
 
-                <fieldset>
-                    <label for="password" class="text-lg mr-2">Senha</label>
-                    <input type="password" id="password" class="w-1/2 border-2 rounded-lg mb-3 p-1.5 outline-none">
+                <fieldset class="w-4/5 mx-auto sm:w-3/5">
+                    <label for="password" class="text-lg pb-1 mr-2 text-gray-600 text-start block">Senha</label>
+                    <input type="password" id="password" class="required w-full border-2 rounded-lg mb-3 p-1.5 outline-none">
+                    <span class="span-required"></span>
                 </fieldset>
 
                 <button type="submit" class="bg-lime-600 text-white text-lg font-semibold mt-4 py-2 px-16 rounded-lg hover:bg-blue-800 hover:text-yellow-400">Continuar</button>
